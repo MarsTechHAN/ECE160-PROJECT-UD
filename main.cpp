0b035f01b6c1cde56508858049b97371d79d64bf
Han <hans3@rose-hulman.edu>
2017-10-07 21:48:42 -0400

    Add servo

--- main.cpp
+++ main.cpp
@@ -1,9 +1,19 @@
-#include <Arduino>
+#include <Servo.h>
 
-void setup(){
+Servo servoL;
+Servo servoR;
 
+
+
+void setup()
+{
+    Serial.begin(115200);
+    Serial.println("PRGM START #");
+    servoR.attach(12);
+    servoL.attach(13);
 }
 
-void loop(){
-    
+void loop()
+{
+   
 }
