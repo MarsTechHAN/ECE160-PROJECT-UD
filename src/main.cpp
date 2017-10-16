a7007faf3d1d786975d9ab8a6c405989c0c5b5eb
Han <hans3@rose-hulman.edu>
2017-10-16 12:33:38 -0400

    Add PS2 joystick control, tested

--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,94 @@
-/*================================================*/
+/*====================INC FILE==================*/
 #include <Arduino.h>
 #include <Servo.h>
-/*================================================*/
-const float SERVO_CALI = 93.0;
+/*=================INC FILE END=================*/
 
-const uint8_t WHEEL_THRESHOLD   = 20;
+/*================GLOBAL DEFINE=================*/
+const float SERVO_CALI = 93.0; //SERVO MID POINT CALI NUMBER
 
+const uint8_t WHEEL_THRESHOLD   = 20; //SERVO FLEX REGION 
+
+#define USE_CURVED_THROTT
+#define USE_CURVED_TURN
+
+#define USE_BLUETOOTH_JOYSTICK
+//#define USE_LORA_JOYSTICK
+//#define USE_IR_REMOTE
+//#define USE_NRF24_JOYSTICK
+
+//#define ENABLE_AUTO_MODE
+
+#define EN_DEBUG
+
+#ifdef EN_DEBUG
+    #define DEBUG_PRINT(str) \
+    Serial.print(millis()); \
+    Serial.print(": "); \
+    Serial.print(__FUNCTION__); \
+    Serial.print("() in "); \
+    Serial.print(__FILE__); \
+    Serial.print(':'); \
+    Serial.print(__LINE__); \
+    Serial.print(' '); \
+    Serial.println(str);
+#else
+    #define DEBUG_PRINT(str)
+#endif
+
+#ifdef USE_CURVED_THROTT
+    #define THROTTCALU(x) (6.0*pow(10.0, -5)*((x) * (x) * (x)) + double(0.1916)*(x))
+#endif
+
+#ifdef USE_CURVED_TURN
+    #define CURVECALU(x) (6.0*pow(10.0, -5)*((x) * (x) * (x)) + double(0.1916)*(x))
+#endif
+
+#ifndef ENABLE_AUTO_MODE
+    #warning "############AUTOMATION MODE DISABLED##################"
+#endif
+
+#ifdef USE_BLUETOOTH_JOYSTICK
+#include <PS2X_lib.h> 
+#endif
+/*==============GLOBAL DEFINE END===============*/
+
+/*=================PIN DEFINE===================*/
 const uint8_t SERVO_LEFT_PIN    = 13;
 const uint8_t SERVO_RIGHT_PIN   = 12;
 
 const uint8_t SERVO_GRAP_PIN    = 11;
 
-Servo servoL;
+#ifdef USE_BLUETOOTH_JOYSTICK
+    const uint8_t PS2X_CLK      = 5;
+    const uint8_t PS2X_CMD      = 3;
+    const uint8_t PS2X_ATT      = 4;
+    const uint8_t PS2X_DAT      = 2;
+#endif
+/*===============PIN DEFINE END==================*/
 
+
+/*==============INSTANLIZATION==================*/
+Servo servoL;
 Servo servoR;
 Servo servoG;
+#ifdef USE_BLUETOOTH_JOYSTICK
+    PS2X ps2x;
+    int8_t CALI_ZERO_LX = 0;
+    int8_t CALI_ZERO_LY = 0;
+    int8_t CALI_ZERO_RX = 0;
+    int8_t CALI_ZERO_RY = 0;
+#endif
+/*============INSTANLIZATION END===================*/
 
+/*==============INIT FUNCTION======================*/
 void vInitServo(){
     servoL.attach(SERVO_LEFT_PIN);
     servoR.attach(SERVO_RIGHT_PIN);
     servoG.attach(SERVO_GRAP_PIN);
 }
+/*==============INIT FUNCTION END==================*/
 
+/*==============SERVO CLASS========================*/
 void vServoForward(float speed){
     servoL.write(SERVO_CALI + speed * WHEEL_THRESHOLD);
     servoR.write(SERVO_CALI - speed * WHEEL_THRESHOLD );
@@ -38,8 +105,8 @@ void vServoSpin(float speed){
 }
 
 void vServoTurn(float speed, float bias){
-    servoL.write(SERVO_CALI + speed * WHEEL_THRESHOLD * bias);
-    servoR.write(SERVO_CALI - speed * WHEEL_THRESHOLD);
+    servoL.write(SERVO_CALI + speed * WHEEL_THRESHOLD +  WHEEL_THRESHOLD * bias);
+    servoR.write(SERVO_CALI - speed * WHEEL_THRESHOLD +  WHEEL_THRESHOLD * bias);
 }
 
 void vServoGrab(){
@@ -49,57 +116,74 @@ void vServoGrab(){
 void vServoDegrab(){
     servoG.write(0);
 }
+/*=================SERVO CALSS END=================*/
 
+#ifdef USE_BLUETOOTH_JOYSTICK
+    void vBLEJoystickCalib(){
+        ps2x.read_gamepad(); 
+        CALI_ZERO_LX = 128 - ps2x.Analog(PSS_LX);
+        CALI_ZERO_LY = 128 - ps2x.Analog(PSS_LY);
+        CALI_ZERO_RX = 128 - ps2x.Analog(PSS_RX);
+        CALI_ZERO_RY = 128 - ps2x.Analog(PSS_RY);
+        DEBUG_PRINT(String("OFFSET: LX")+String(CALI_ZERO_LX) +
+                        String(" LY")+String(CALI_ZERO_LY) +
+                            String(" RX")+String(CALI_ZERO_RX) +
+                                String(" RY")+String(CALI_ZERO_RY));
+    }
+#endif
 void setup()
 {
     Serial.begin(115200);
-    Serial.println("PRGM START #");
+    DEBUG_PRINT("PROGRAM START, BANDRATE INIT TO 115200");
+    
     
     vInitServo();
+
+    #ifdef USE_BLUETOOTH_JOYSTICK
+        uint8_t error = ps2x.config_gamepad(PS2X_CLK, PS2X_CMD, PS2X_ATT, PS2X_DAT, false, false);
+        if(0 == error){
+            DEBUG_PRINT("PS2X JOYSTICK INIT SUCCESS");
+        }else{
+            DEBUG_PRINT(String("PS2X JOYSTICK INIT FAIL, ERROR CODE: ") + error);
+            while(1);
+        }
+        vBLEJoystickCalib();
+    #endif
+    
 }
 
 uint64_t u64SysTick = 0;
+bool bIsGrab = false;
 
 void loop()
 {
-        while(analogRead(A0) > 600){
-            vServoBackward(0);
-            vServoGrab();
-            delay(50);
-            Serial.println(analogRead(A0));
-            delay(50);
+    #ifdef USE_BLUETOOTH_JOYSTICK
+        ps2x.read_gamepad(); 
+        float fForwardSpeed = float(128 - (CALI_ZERO_LY + ps2x.Analog(PSS_LY))) / 512;
+        float fTurnSpeed = (float((CALI_ZERO_RX + ps2x.Analog(PSS_RX) - 128)) / 256);
+        if(fTurnSpeed < 0.1){
+            fForwardSpeed *= 2;
         }
-
-        if((millis() < u64SysTick+5000)){
-            vServoDegrab();
-            vServoForward(0.8);
+        if(true == ps2x.Button(PSB_L1)){
+            bIsGrab = false;
         }
-
-        if((millis() > u64SysTick+5000) && (millis() < u64SysTick+10000)){
-            vServoDegrab();
-            vServoBackward(0.8);
+        if(true == ps2x.Button(PSB_R1)){
+            bIsGrab = true;
         }
+    #endif
+    
+    #ifdef USE_CURVED_THROTT
+        fForwardSpeed   = THROTTCALU(fForwardSpeed);
+        fTurnSpeed      = CURVECALU(fTurnSpeed);
+    #endif
 
-        if((millis() > u64SysTick+10000) && (millis() < u64SysTick+15000)){
-            vServoGrab();
-            vServoTurn(0.2, 1.8);
-        }
-
-        if((millis() > u64SysTick+15000) && (millis() < u64SysTick+20000)){
-            vServoTurn(-0.2, 1.8);
-        }
-
-        if((millis() > u64SysTick+20000) && (millis() < u64SysTick+25000)){
-            vServoSpin(1);
-        }
-
-        if((millis() > u64SysTick+25000) && (millis() < u64SysTick+30000)){
-            vServoDegrab();
-            vServoSpin(-1);
-        }
-
-        if((millis() > u64SysTick+30000)){
-            u64SysTick = millis();
-        }        
-
+    vServoTurn(fForwardSpeed, fTurnSpeed);
+    if(true == bIsGrab){
+        vServoGrab();
+    }else{
+        vServoDegrab();
+    }
+    DEBUG_PRINT(String("Forward Speed: ") + String(fForwardSpeed) + 
+                        String(" TurnSpeed: ") + String(fTurnSpeed) + 
+                            String(" Grab Stuatus: ") + String(bIsGrab));
 }
